5e258ef35550c461adbfe4f08dd56708fd21d1fd
zoz0312 <zoz0312@naver.com>
2020-09-07 22:44:54 +0900

    Style 디렉터리 구조 변경

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './Header.scss';
+
+const Header: React.FC = () => {
+  return (
+		<header className="header">
+			Header
+		</header>
+  );
+}
+
+export default Header;
