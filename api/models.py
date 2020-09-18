052f2a84a831c940cef4d6d8fe12b57b0d9cea12
Ubuntu <ubuntu@ip-172-31-9-170.ap-northeast-2.compute.internal>
2020-09-18 12:33:46 +0000

    add member

--- api/models.py
+++ api/models.py
@@ -3,6 +3,13 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+class Member(models.Model):
+    idx = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    class Meta:
+        db_table = 'member'
+
 class Feeds(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=144)
