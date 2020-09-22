97e9896dd77be4b3887596718b44e39aa25718d4
root <root@ip-172-31-9-170.ap-northeast-2.compute.internal>
2020-09-22 00:14:31 +0000

    add require

--- require.sh
+++ require.sh
@@ -7,5 +7,7 @@ pip install mysqlclient;\
 pip install djangorestframework;\
 pip install mysqlclient;\
 pip install django-webpack-loader;\
+pip install jwt;\
+pip install bcrypt;\
 python -m pip install Pillow;\
 
